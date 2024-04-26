190b8971172abc3b98f2da7c35d046ef94262f96
Rafael de Cerqueira <rafael.f.l.cerqueira@gmail.com>
2024-04-26 12:41:36 -0300

    Add trainining scripts

---
+++ UNET/training/simple/read_dataset.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+from pathlib import Path
+import os
+
+
+def read_dataset(dataset_folder, window_size=256, subset="Training", debug=False):
+    """
+        This function reade the dataset from a dataset_folder.
+    """
+
+    images_folder = Path(dataset_folder, subset, 'images')
+    masks_folder = Path(dataset_folder, subset, 'masks')
+
+    all_images = os.listdir(images_folder)
+    all_masks = os.listdir(masks_folder)
+
+    N_images = len(all_images)
+    N_masks = len(all_masks)
+
+    images = np.zeros((N_images, window_size, window_size, 1), dtype=np.uint8)
+    masks = np.zeros((N_masks, window_size, window_size, 1), dtype=np.uint8)
+
+    for i in range(N_images):
+        img_base_name = all_images[i].split('.')[0]
+
+        img_image = cv2.imread(str(Path(images_folder, "%s.jpg" % img_base_name)), 0)
+        img_mask = cv2.imread(str(Path(masks_folder, "%s.png" % img_base_name)), 0)
+
+        img_image = cv2.resize(img_image, (window_size, window_size))
+        img_mask = cv2.resize(img_mask, (window_size, window_size))
+
+        if debug:
+            cv2.namedWindow('tst', cv2.WINDOW_NORMAL)
+            cv2.imshow('tst', np.hstack((img_image, img_mask)))
+            cv2.waitKey(0)
+
+        images[i, :, :, 0] = img_image
+        masks[i, :, :, 0] = img_mask
+
+    images = images.astype(float)
+    masks = masks.astype(float)
+    images /= 255.0
+    masks /= 255.0
+
+    return images, masks
---
+++ UNET/training/simple/train.py
@@ -0,0 +1,108 @@
+import os
+from pathlib import Path
+import cv2
+import numpy as np
+import keras
+
+from keras.models import Model, load_model
+from keras.layers import Input, BatchNormalization, Activation, Dense, Dropout
+from keras.layers import Conv2D, Conv2DTranspose
+from keras.layers import MaxPooling2D, GlobalMaxPool2D
+from keras.layers import concatenate, add
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping, Callback
+import sys
+
+from read_dataset import read_dataset
+
+from conv2d_block import conv2d_block
+
+class TrackProgress(Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        out_progress_folder = Path("progress/best")
+        if epoch == 0:
+            out_progress_folder.mkdir(parents=True, exist_ok=True)
+        if epoch % 10 == 0:
+            _images = images_val[::16, :, :].copy()
+            _masks = masks_val[::16, :, :].copy()
+
+            n_tests = _images.shape[0]
+            _pred_masks = model.predict(_images)
+
+            _images *= 255.0
+            _masks *= 255.0
+            _pred_masks *= 255.0
+
+            for i in range(n_tests):
+                _img = _images[i, :, :, 0].astype(np.uint8)
+                _mask = _masks[i, :, :, 0].astype(np.uint8)
+                _pred_mask = _pred_masks[i, :, :, 0].astype(np.uint8)
+                _out_img = np.hstack((_img, _mask, _pred_mask))
+                cv2.imwrite(
+                    str(
+                    Path(out_progress_folder, f'progress_img_ex_{i:03d}_{epoch:05d}.jpg')
+                    ),
+                    _out_img,
+                )
+
+
+dataset_folder = Path("../../dataset/dataset_UNET")
+window_size = 64
+EPOCHS = 5000
+PATIENCE = 500
+BATCH_SIZE = 128
+
+images_train, masks_train = read_dataset(
+    dataset_folder, window_size=window_size, subset="Training"
+)
+images_val, masks_val = read_dataset(
+    dataset_folder, window_size=window_size, subset="Validation"
+)
+
+N_dataset = images_train.shape[0]
+shuffle = np.arange(N_dataset)
+np.random.seed(13)
+np.random.shuffle(shuffle)
+images_train = images_train[shuffle, :, :, :]
+masks_train = masks_train[shuffle, :, :, :]
+
+input_img = Input((window_size, window_size, 1), name="img")
+
+c1 = conv2d_block(input_img, 8, kernel_size=5, batchnorm=True)
+p1 = MaxPooling2D((4, 4))(c1)
+
+c2 = conv2d_block(p1, 8, kernel_size=5, batchnorm=True)
+p2 = MaxPooling2D((4, 4))(c2)
+
+c3 = conv2d_block(p2, 8, kernel_size=5, batchnorm=True)
+
+u4 = Conv2DTranspose(8, kernel_size=5, strides=(4, 4), padding="same")(c3)
+u4 = concatenate([u4, c2])
+c4 = conv2d_block(u4, 8, kernel_size=5, batchnorm=True)
+
+u5 = Conv2DTranspose(8, kernel_size=5, strides=(4, 4), padding="same")(c4)
+u5 = concatenate([u5, c1])
+c5 = conv2d_block(u5, 8, kernel_size=5, batchnorm=True)
+
+outputs = Conv2D(1, (1, 1), activation="sigmoid")(c5)
+model = Model(inputs=[input_img], outputs=[outputs])
+model.compile(optimizer=Adam(), loss="binary_crossentropy")
+
+model.summary()
+
+early_stopping = EarlyStopping(
+    monitor="val_loss", mode="min", verbose=1, patience=PATIENCE
+)
+
+checkpoint = ModelCheckpoint(
+    "UNET_best.keras", verbose=1, save_best_only=True, monitor="val_loss", mode="auto"
+)
+
+model.fit(
+    images_train,
+    masks_train,
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
+    validation_data=(images_val, masks_val),
+    callbacks=[checkpoint, early_stopping, TrackProgress()],
+)
