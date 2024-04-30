fb274f9bad18978f41af92c3cdbd1424d81a9a4f
Rafael de Cerqueira <rafael.f.l.cerqueira@gmail.com>
2024-04-30 18:40:22 -0300

    Improve and refactor code

--- UNET/prediction/recreate_UNET_image.py
+++ UNET/prediction/recreate_UNET_image.py
@@ -2,8 +2,6 @@ import cv2
 import numpy as np
 
 
-# import config as cfg
-
 def recreate_UNET_image(subdivided_image_UNET, window_size, raw_img, stride_division=2):
     UNET_image = np.zeros_like(raw_img, dtype=float)
     UNET_image_nmax = np.zeros_like(raw_img, dtype=float)
@@ -19,7 +17,6 @@ def recreate_UNET_image(subdivided_image_UNET, window_size, raw_img, stride_divi
     scan_I = np.arange(0, raw_img.shape[0] - window_size + 1, window_size // stride_division)
     scan_J = np.arange(0, raw_img.shape[1] - window_size + 1, window_size // stride_division)
 
-    # Generating the U-Net image
     index = 0
     for i in scan_I:
         for j in scan_J:
@@ -28,21 +25,12 @@ def recreate_UNET_image(subdivided_image_UNET, window_size, raw_img, stride_divi
             j_start = j
             j_end = j + window_size
 
-            # Returning the 'intermediary' U-Net from the sliding window full matrix
-            # See that I am summing this 'sub-image' into the original/desired U-Net image
-            # That is the 'U-Net numerator'
             sub_window = cv2.resize(subdivided_image_UNET[index, :, :, 0], (window_size, window_size))
             UNET_image[i_start:i_end, j_start:j_end] += sub_window
-            # UNET_image[i_start:i_end, j_start:j_end] = subdivided_image_UNET[index, :, :, 0]
 
-            # Since we are summing in the numerator, and the values are bounded to 0 and 255
-            # I have a second matrix, the UNET_image_nmax, which is a denominator
-            # Therefore, I am updating the denominator here
             UNET_image_nmax[i_start:i_end, j_start:j_end] += 255.0
             index += 1
 
-    # print("PROBLEM WHILE GENERATING U-NET IMAGES. FIX IT!!! USE 1 STRIDE_DIV")
-    #    # Now I divide the image
     UNET_image /= UNET_image_nmax
     UNET_image /= np.nanmax(UNET_image)
     UNET_image *= 255.0
--- UNET/prediction/run.py
+++ UNET/prediction/run.py
@@ -14,7 +14,6 @@ from UNET.prediction.recreate_UNET_image import recreate_UNET_image
 image_file_folder = r'/EXPERIMENTS/HSC_images/22_08_Ql_1_P_1_HIGH_20__001'
 
 UNET_model_file = os.path.join("../training/simple/UNET_best.keras")
-print(os.getcwd())
 UNET_model = keras.models.load_model(UNET_model_file, compile=False)
 window_size = 128
 sub_image_size = 64
@@ -49,14 +48,3 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
 
     for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc="Processing images"):
         future.result()
-
-# for index_img, image_file in enumerate(tqdm(image_files, desc="Applying U-Net model")):
-#
-#     img = cv2.imread(str(Path(image_file_folder, image_file)),0)
-#     subdivided_image_raw = divide_image(window_size, sub_image_size, img, stride_division)
-#
-#     subdivided_image_UNET = apply_UNET_mask(subdivided_image_raw, UNET_model)
-#     img_UNET = recreate_UNET_image(subdivided_image_UNET, window_size, img, stride_division)
-#
-#     out_img = np.hstack((img, img_UNET))
-#     cv2.imwrite(str(Path(out_folder, f'img_{index_img:06d}.jpg')), out_img)
