73efe3f347a40c28fb5f3c5c6a30c0e3e5da7fd1
higor0227 <higor0227@gmail.com>
2024-06-07 16:52:12 -0300

    Add ellipse cv contour

--- test/random_circle_generator.py
+++ test/random_circle_generator.py
@@ -6,32 +6,39 @@ import os
 import h5py
 
 
-def create_random_dataset(image_width, image_height, N_IMAGES, output_path):
+# ellipse_bounds = [ellipse_width_bounds, ellipse_height_bounds, ellipse_ahgle_bounds]
+# ellipse_bounds = [(x, y), (w, z), (a, b)]
+def create_random_dataset(image_width, image_height, N_IMAGES, output_path, ellipse_bounds):
     output_path = Path(output_path, "output")
     output_path.mkdir(parents=True, exist_ok=True)
 
     for i in range(0, N_IMAGES):
+        contours = []
+
         img = np.zeros((image_height, image_width, 3), dtype=np.uint8)
         N_ELLIPSES = random.randint(15, 35)
 
         for j in range(0, N_ELLIPSES):
-            ellipse_center = (random.randint(0, image_width - 1), random.randint(0, image_height - 1))
-            ellipse_width = random.randint(15, 20)
-            ellipse_height = random.randint(15, 20)
-            ellipse_angle = random.randint(0, 30)
+            ellipse_center = (random.randint(0, image_width), random.randint(0, image_height))
+            ellipse_width = random.randint(ellipse_bounds[0][0], ellipse_bounds[0][1])
+            ellipse_height = random.randint(ellipse_bounds[1][0], ellipse_bounds[1][1])
+            ellipse_angle = random.randint(ellipse_bounds[2][0], ellipse_bounds[2][1])
 
             img = cv2.ellipse(img, ellipse_center,
                               (ellipse_width, ellipse_height),
                               ellipse_angle, 0, 360, (255, 255, 255), -1)
 
-        # Converter a imagem para escala de cinza
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            ellipse_contour = ellipse_to_contour(ellipse_center, ellipse_width, ellipse_height, ellipse_angle)
+            contours.append(ellipse_contour)
 
-        contours, _ = cv2.findContours(gray_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
         cv2.imwrite(
-            str(Path(output_path, f"img_{i:03d}.png")), img
+            str(Path(output_path, f"{i:03d}.png")), img
         )
+
+        print(N_ELLIPSES, len(contours))
 
         export_h5(output_path, img, contours, i)
 
@@ -51,7 +58,21 @@ def export_h5(output_path, img, contours, img_index):
             contours_group.create_dataset(f"cnt_{idx:06d}", data=contour)
 
 
+def ellipse_to_contour(center, width, height, angle):
+    ellipse_poly = cv2.ellipse2Poly(center, (width, height), int(angle), 360, 1, 1)
+
+    N_points = len(ellipse_poly)
+    cv2_contour = np.zeros((N_points, 1, 2), dtype=int)
+
+    for i, (x, y) in enumerate(ellipse_poly):
+        cv2_contour[i, 0, 0] = ellipse_poly[i][0]
+        cv2_contour[i, 0, 1] = ellipse_poly[i][1]
+
+    return cv2_contour
+
+
 image_width = 252
 image_height = 1024
 path = Path(os.getcwd())
-create_random_dataset(image_width, image_height, 4, path)
+ellipse_bounds = [[15, 20], [15, 20], [0, 30]]
+create_random_dataset(image_width, image_height, 10, path, ellipse_bounds)
