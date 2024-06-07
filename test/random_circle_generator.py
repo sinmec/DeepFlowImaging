cc3ec597d1ce24bb716bc6fe791df12c849d91c3
higor0227 <higor0227@gmail.com>
2024-06-07 16:14:42 -0300

    add h5 export

--- test/random_circle_generator.py
+++ test/random_circle_generator.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 import cv2
 import os
+import h5py
 
 
 def create_random_dataset(image_width, image_height, N_IMAGES, output_path):
@@ -10,52 +11,45 @@ def create_random_dataset(image_width, image_height, N_IMAGES, output_path):
     output_path.mkdir(parents=True, exist_ok=True)
 
     for i in range(0, N_IMAGES):
-        img = np.zeros((image_height, image_width), dtype=np.uint8)
+        img = np.zeros((image_height, image_width, 3), dtype=np.uint8)
         N_ELLIPSES = random.randint(15, 35)
 
         for j in range(0, N_ELLIPSES):
-            ellipse_center = (random.randint(0, image_width), random.randint(0, image_height))
+            ellipse_center = (random.randint(0, image_width - 1), random.randint(0, image_height - 1))
             ellipse_width = random.randint(15, 20)
             ellipse_height = random.randint(15, 20)
             ellipse_angle = random.randint(0, 30)
 
             img = cv2.ellipse(img, ellipse_center,
-                        (int(ellipse_width), int(ellipse_height)),
-                        ellipse_angle, 0, 360, (255, 255, 255), -1)
+                              (ellipse_width, ellipse_height),
+                              ellipse_angle, 0, 360, (255, 255, 255), -1)
 
-        contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Converter a imagem para escala de cinza
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        contours, _ = cv2.findContours(gray_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         cv2.imwrite(
             str(Path(output_path, f"img_{i:03d}.png")), img
         )
 
+        export_h5(output_path, img, contours, i)
 
-def export_h5():
-    h5_file_name = f"{image_folder_name}_{self.number_of_exports:06d}.h5"
-    h5_file_path = Path(self.output_path, h5_file_name)
 
-    h5_file = h5py.File(h5_file_path, "w")
-    h5_file.attrs['date'] = datetime.now().strftime('%Y_%m_%d_%H_%M')
+def export_h5(output_path, img, contours, img_index):
+    h5_file_name = "test.h5"
+    h5_file_path = Path(output_path, h5_file_name)
 
-    current_image = self.image_files[self.file_index]
-    img_group = h5_file.create_group(f"{current_image.name}")
-    img_group.create_dataset(
-        "img", data=np.array(cv2.imread(str(current_image), 1))
-    )
-    contours_group = img_group.create_group("contours")
+    with h5py.File(h5_file_path, "a") as h5_file:
+        img_group_name = f"img_{img_index:03d}"
+        img_group = h5_file.create_group(img_group_name)
 
-    n_contours = 0
-    for item in range(len(self.contour_collection.items)):
-        contour = self.contour_collection.items[item]
-        if not contour.valid:
-            continue
-        if contour.navigation_window_contour is None:
-            continue
-        cv2_contours = contour.navigation_window_contour
-        contours_group.create_dataset(f"cnt_{n_contours:06d}", data=np.array(cv2_contours))
-        n_contours += 1
+        img_group.create_dataset("img", data=img)
 
-    h5_file.close()
+        contours_group = img_group.create_group("contours")
+        for idx, contour in enumerate(contours):
+            contours_group.create_dataset(f"cnt_{idx:06d}", data=contour)
+
 
 image_width = 252
 image_height = 1024
